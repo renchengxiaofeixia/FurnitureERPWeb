4e7b6b8b009dc44254f17552e9a1869b21e71b88
1@1.1 <1@1.1>
2023-06-24 11:52:10 +0800

    改掉合并的错误

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,11 @@ import { configureStore } from "@reduxjs/toolkit"
 import counter from "./modules/counter"
 import login from "./modules/login"
 import app from "./modules/app"
-import item from "./modules/item"
 
 export default configureStore({
     reducer:{
         counter,
         login,
-        app,
-        item
+        app
     }
 })
